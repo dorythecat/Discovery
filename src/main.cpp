dd331e32ebba64d29a10dced38155d6d7c9cb96d
dorythecat <pemegomachado@gmail.com>
2026-03-09 18:22:23 +0100

    Created first (test) window

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,57 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
 #include <iostream>
-using namespace std;
+#include <stdexcept>
+#include <cstdlib>
+
+constexpr uint32_t SCR_WIDTH = 800;
+constexpr uint32_t SCR_HEIGHT = 600;
+
+class HelloTriangleApplication {
+public:
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    GLFWwindow* window;
+
+    void initWindow() {
+        glfwInit();
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // TODO: Make resizable
+
+        window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Testing", nullptr, nullptr);
+    }
+
+    void initVulkan() {
+
+    }
+
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup() {
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+    }
+};
 
 int main() {
-    // This statement prints "Hello World"
-    cout << "Hello World";
-
-    return 0;
+    try {
+        HelloTriangleApplication app;
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    } return EXIT_SUCCESS;
 }
