fea128f98442e797cb5b607520b3cbc0a6caac69
dorythecat <pemegomachado@gmail.com>
2026-03-11 19:14:16 +0100

    Set up image views

--- src/main.cpp
+++ src/main.cpp
@@ -58,9 +58,10 @@ private:
     VkQueue _presentQueue;
 
     VkSwapchainKHR _swapChain;
-    std::vector<VkImage> _swapChainImages;
     VkFormat _swapChainImageFormat;
     VkExtent2D _swapChainExtent;
+    std::vector<VkImage> _swapChainImages;
+    std::vector<VkImageView> _swapChainImageViews;
 
     // Main functions
     void initWindow() {
@@ -79,6 +80,7 @@ private:
         pickPhysicalDevice();
         createLogicalDevice();
         createSwapChain();
+        createImageViews();
     }
 
     void mainLoop() const {
@@ -88,6 +90,7 @@ private:
     }
 
     void cleanup() const {
+        for (auto imageView : _swapChainImageViews) vkDestroyImageView(_device, imageView, nullptr);
         vkDestroySwapchainKHR(_device, _swapChain, nullptr);
 
         vkDestroyDevice(_device, nullptr);
@@ -271,6 +274,33 @@ private:
 
         _swapChainImageFormat = surfaceFormat.format;
         _swapChainExtent = extent;
+    }
+
+    void createImageViews() {
+        _swapChainImageViews.resize(_swapChainImages.size());
+
+        for (size_t i = 0; i < _swapChainImages.size(); i++) {
+            VkImageViewCreateInfo createInfo{};
+            createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+            createInfo.image = _swapChainImages[i];
+
+            createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+            createInfo.format = _swapChainImageFormat;
+
+            createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+
+            createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+            createInfo.subresourceRange.baseMipLevel = 0;
+            createInfo.subresourceRange.levelCount = 1;
+            createInfo.subresourceRange.baseArrayLayer = 0;
+            createInfo.subresourceRange.layerCount = 1;
+
+            if (vkCreateImageView(_device, &createInfo, nullptr, &_swapChainImageViews[i]) != VK_SUCCESS)
+                logger.log(FATAL, "Failed to create image views!");
+        }
     }
 
     // Helper functions
